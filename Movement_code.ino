a8ef2b564f51c7e82d61a18d012fe1ca5a1537a4
Hari Om Chadha <chadhahariom1@gmail.com>
2024-02-18 10:49:26 -0500

    Add files via upload

--- Movement_code.ino
+++ Movement_code.ino
@@ -1,3 +1,122 @@
+// int melody[] = { 262,196, 196, 220, 196, 0,  247, 262
+//   /*NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3,0, NOTE_B3, NOTE_C4*/ };
+
+// // note durations: 4 = quarter note, 8 = eighth note, etc.:
+// int noteDurations[] = {
+//   4, 8, 8, 4,4,4,4,4 };
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST      0
+
+int melody[] = { NOTE_GS3, NOTE_AS3, NOTE_CS4, NOTE_AS3, NOTE_F4, NOTE_F4, NOTE_DS4,
+                 NOTE_GS3, NOTE_AS3, NOTE_CS4, NOTE_AS3, NOTE_DS4, NOTE_DS4, NOTE_CS4,
+                 NOTE_GS3, NOTE_AS3, NOTE_CS4, NOTE_AS3, NOTE_CS4, NOTE_DS4, NOTE_C4, NOTE_AS3, NOTE_GS3, 
+                 NOTE_GS3, NOTE_DS4, NOTE_CS4,
+  };
+
+int len = 26;
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+                8, 8, 8, 8,
+                2.67, 2.67, 4,
+                8, 8, 8, 8,
+                2.67, 2.67, 1.3,
+                8, 8, 8, 8, 
+                2, 4, 2.67, 8, 2,
+                4, 2, 4
+ };
+
+int LEDPin = 3;
+
 int right1 = 8;
 int right2 = 9;
 
@@ -6,18 +125,79 @@ int left2 = 11;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(right1, OUTPUT);
   pinMode(right2, OUTPUT);
   pinMode(left1, OUTPUT);
   pinMode(left2, OUTPUT);
+  pinMode(LEDPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //forward();
-  //backward();
-  //right();
-  //left();
+  String input = " ";
+  if (Serial.available() > 0) {
+    input = Serial.readString();  // Read the incoming data
+    input.trim(); // Trim any whitespace
+    if (input == "sound"){
+      play();
+    }else {
+    controlMotors(input); // Parse and execute the command
+    } 
+  }
+
+}
+
+void light_on(){
+  digitalWrite(LEDPin, HIGH);
+}
+
+void light_off(){
+  digitalWrite(LEDPin, LOW);
+}
+
+
+void play() {
+    for (int thisNote = 0; thisNote < len; thisNote++) {
+
+    // to calculate the note duration, take one second 
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(7, melody[thisNote],noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(7);
+  }
+}
+
+void controlMotors(String input) {
+  int firstSpace = input.indexOf(' ');
+  int secondSpace = input.indexOf(' ', firstSpace + 1);
+  
+  if (firstSpace != -1 && secondSpace != -1) {
+    String direction = input.substring(0, firstSpace);
+    String durationStr = input.substring(firstSpace + 1, secondSpace);    
+    double duration = durationStr.toDouble();
+    
+    if (direction == "forward") {
+      forward();
+    } else if (direction == "backward") {
+      backward();
+    } else if (direction == "right") {
+      right();
+    } else if (direction == "left") {
+      left();
+    } else {
+      stop(); // Stop if direction is unknown
+    }
+    
+    delay(duration * 1000); // Convert duration to milliseconds and delay
+    stop(); // Stop the motors after the duration
+  }
 }
 
 
@@ -48,4 +228,84 @@ void left(){
   digitalWrite(left1, HIGH);
   digitalWrite(left2, LOW);
   
+}
+
+void stop(){
+  digitalWrite(right1, LOW);
+  digitalWrite(right2, LOW);
+  digitalWrite(left1, LOW);
+  digitalWrite(left2, LOW);
+}
+
+
+// timers TC0 TC1 TC2   channels 0-2 ids 0-2  3-5  6-8     AB 0 1
+// use TC1 channel 0 
+#define TONE_TIMER TC1
+#define TONE_CHNL 0
+#define TONE_IRQ TC3_IRQn
+
+// TIMER_CLOCK4   84MHz/128 with 16 bit counter give 10 Hz to 656KHz
+//  piano 27Hz to 4KHz
+
+static uint8_t pinEnabled[PINS_COUNT];
+static uint8_t TCChanEnabled = 0;
+static boolean pin_state = false ;
+static Tc *chTC = TONE_TIMER;
+static uint32_t chNo = TONE_CHNL;
+
+volatile static int32_t toggle_count;
+static uint32_t tone_pin;
+
+// frequency (in hertz) and duration (in milliseconds).
+
+void tone(uint32_t ulPin, uint32_t frequency, int32_t duration)
+{ 
+    if (frequency != 0){
+      light_on();
+    }
+    const uint32_t rc = VARIANT_MCK / 256 / frequency; 
+    tone_pin = ulPin;
+    toggle_count = 0;  // strange  wipe out previous duration
+    if (duration > 0 ) toggle_count = 2 * frequency * duration / 1000;
+     else toggle_count = -1;
+
+    if (!TCChanEnabled) {
+      pmc_set_writeprotect(false);
+      pmc_enable_periph_clk((uint32_t)TONE_IRQ);
+      TC_Configure(chTC, chNo,
+        TC_CMR_TCCLKS_TIMER_CLOCK4 |
+        TC_CMR_WAVE |         // Waveform mode
+        TC_CMR_WAVSEL_UP_RC ); // Counter running up and reset when equals to RC
+  
+      chTC->TC_CHANNEL[chNo].TC_IER=TC_IER_CPCS;  // RC compare interrupt
+      chTC->TC_CHANNEL[chNo].TC_IDR=~TC_IER_CPCS;
+       NVIC_EnableIRQ(TONE_IRQ);
+                         TCChanEnabled = 1;
+    }
+    if (!pinEnabled[ulPin]) {
+      pinMode(ulPin, OUTPUT);
+      pinEnabled[ulPin] = 1;
+    }
+    TC_Stop(chTC, chNo);
+                TC_SetRC(chTC, chNo, rc);    // set frequency
+    TC_Start(chTC, chNo);
+}
+
+void noTone(uint32_t ulPin)
+{
+  light_off();
+  TC_Stop(chTC, chNo);  // stop timer
+  digitalWrite(ulPin,LOW);  // no signal on pin
+}
+
+// timer ISR  TC1 ch 0
+void TC3_Handler ( void ) {
+  TC_GetStatus(TC1, 0);
+  if (toggle_count != 0){
+    // toggle pin  TODO  better
+    digitalWrite(tone_pin,pin_state= !pin_state);
+    if (toggle_count > 0) toggle_count--;
+  } else {
+    noTone(tone_pin);
+  }
 }
